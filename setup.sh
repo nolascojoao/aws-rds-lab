e238846431b8dcf9e8ec706de873480beb6710c6
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-10-20 22:14:55 -0300

    Add setup.sh script to install MariaDB client

---
+++ setup.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+# Update the instance packages
+sudo dnf update -y
+
+# Install the MariaDB client
+sudo dnf install -y mariadb105-server.x86_64
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+
